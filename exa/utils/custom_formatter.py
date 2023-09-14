8963189f5617f4a733303f156fb81f34b2ce81e4
Kye <kye@apacmediasolutions.com>
2023-09-13 23:37:04 -0400

    logging

--- exa/utils/custom_formatter.py
+++ exa/utils/custom_formatter.py
@@ -1,9 +1,18 @@
 import logging
 from termcolor import colored
 
-
-# Custom logging formatter for color-coded logging
 class CustomFormatter(logging.Formatter):
+    """
+    Custom logging formatter for color-coded logging.
+
+    Provides a custom format for logs based on the level of logging.
+    Each logging level has its own associated color to easily distinguish
+    between different log messages.
+
+    Attributes:
+        format_mappings (dict): Mapping of logging levels to their associated color and format.
+    """
+
     format_mappings = {
         logging.DEBUG: {"color": "grey", "format": "%(asctime)s - %(levelname)s - %(message)s"},
         logging.INFO: {"color": "green", "format": "%(asctime)s - %(levelname)s - %(message)s"},
@@ -12,54 +21,108 @@ class CustomFormatter(logging.Formatter):
     }
 
     def format(self, record):
+        """
+        Format the log message.
+
+        Args:
+            record (LogRecord): The record to be formatted.
+
+        Returns:
+            str: Formatted log message with the appropriate color.
+        """
         format_dict = self.format_mappings.get(record.levelno)
         log_message = super().format(record)
         return colored(log_message, format_dict["color"])
 
-# Colored logging setup
-logging.basicConfig(level=logging.INFO)
-handler = logging.StreamHandler()
-handler.setFormatter(logging.Formatter('%(levelname)s: %(message)s'))
-logger = logging.getLogger(__name__)
-logger.addHandler(handler)
-
-
 class ColoredLogger:
+    """
+    Utility class for colored logging.
+
+    Provides static methods to log messages with colors associated with
+    their log levels. The actual coloring is handled by the CustomFormatter.
+    """
+
     @staticmethod
     def info(msg):
-        logger.info(colored(msg, 'green'))
+        """
+        Log an info level message.
+
+        Args:
+            msg (str): The message to be logged.
+        """
+        logger.info(msg)
 
     @staticmethod
     def warning(msg):
-        logger.warning(colored(msg, 'yellow'))
+        """
+        Log a warning level message.
+
+        Args:
+            msg (str): The message to be logged.
+        """
+        logger.warning(msg)
 
     @staticmethod
     def error(msg):
-        logger.error(colored(msg, 'red'))
+        """
+        Log an error level message.
+
+        Args:
+            msg (str): The message to be logged.
+        """
+        logger.error(msg)
 
     @staticmethod
     def debug(msg):
-        logger.debug(colored(msg, 'blue'))
+        """
+        Log a debug level message.
 
+        Args:
+            msg (str): The message to be logged.
+        """
+        logger.debug(msg)
 
-# Decorator to log method entry and exit
-def log_metadata(cls):
+def log_method_calls(cls):
     """
-    @log_metadata
+    Decorator to log method entry and exit for classes.
+    
+    When a method of a decorated class is called, this decorator logs the 
+    method's name upon entering and exiting the method. Useful for debugging
+    and tracking the flow of the program.
+    
+    Usage:
+    @log_method_calls
     class MyClass:
         def say_hello(self):
             print("hello")
         
         def say_goodbye(self):
             print(f"Goodbye!")
+
+    Args:
+        cls (type): The class to be wrapped.
+
+    Returns:
+        type: Wrapped class with added logging functionality.
     """
-            
+
     class Wrapped(cls):
+        """Wrapper class to intercept method calls and log them."""
+
         def __init__(self, *args, **kwargs):
             self._logger = logging.getLogger(cls.__name__)
             super().__init__(*args, **kwargs)
 
         def __getattribute__(self, s):
+            """
+            Intercept method calls to log them.
+
+            Args:
+                s (str): The name of the attribute or method to be accessed.
+
+            Returns:
+                Any: The wrapped method with added logging or the original attribute.
+            """
             attr = super().__getattribute__(s)
 
             if callable(attr):
@@ -68,7 +131,16 @@ def log_metadata(cls):
                     result = attr(*args, **kwargs)
                     self._logger.debug(colored(f'Exiting {s} method', 'blue'))
                     return result
+
                 return wrapped
             else:
                 return attr
+
     return Wrapped
+
+# Set up the logger
+logging.basicConfig(level=logging.INFO)
+handler = logging.StreamHandler()
+handler.setFormatter(CustomFormatter())
+logger = logging.getLogger(__name__)
+logger.addHandler(handler)
