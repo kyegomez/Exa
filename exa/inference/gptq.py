d8a0e0220a4c924e6e3b4a7aa68dbdc10a770751
Kye <kye@apacmediasolutions.com>
2023-09-20 18:23:01 -0400

    real time decoding wrapper

--- exa/inference/gptq.py
+++ exa/inference/gptq.py
@@ -5,6 +5,8 @@ from torch.multiprocessing import set_start_method
 from torch.nn.parallel import DistributedDataParallel as DDP
 from transformers import AutoModelForCausalLM, AutoTokenizer, GPTQConfig
 
+from exa.utils.decoding_wrapper import real_time_decoding
+
 #set up logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -79,7 +81,8 @@ class GPTQInference:
             )
         
         logger.info(f"Model loaded from {self.model_id} on {self.device}")
-
+    
+    @real_time_decoding
     def run(
             self, 
             prompt: str,
