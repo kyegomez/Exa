19ad0b50e56bd29a0dcdbbb278f94c7456bcae71
Kye <kye@apacmediasolutions.com>
2023-09-22 14:58:12 -0400

    cleanup

--- exa/__init__.py
+++ exa/__init__.py
@@ -9,3 +9,4 @@ from exa.inference.mmi import MultiModalInference
 #utils
 from exa.utils.metric_logger import Logging
 from exa.utils.decoding_wrapper import real_time_decoding
+from exa.utils.deploy import Deploy
--- exa/utils/__init__.py
+++ exa/utils/__init__.py
@@ -1,3 +1,4 @@
 #utils
 from exa.utils.metric_logger import Logging
 from exa.utils.decoding_wrapper import real_time_decoding
+from exa.utils.deploy import Deploy
