34beeab94d5bd3950ce433a7b28dfb34298bc7a7
Kye <kye@apacmediasolutions.com>
2023-09-22 15:30:49 -0400

    clean up

--- exa/utils/deploy.py
+++ exa/utils/deploy.py
@@ -1,24 +1,13 @@
-import os
-from typing import List
 
 import uvicorn
-from fastapi import BackgroundTasks, Depends, FastAPI, HTTPException
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import FileResponse
-from fastapi_pagination import PaginationParams, paginate
-from fastapi_pagination.bases import AbstractPage
-from fastapi_pagination.ext.tortoise import paginate
-from loguru import logger
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from slowapi import _rate_limit_exceeded_handler
 from slowapi.errors import RateLimitExceeded
-from slowapi.limiter import Limiter
 from slowapi.middleware import SlowAPIMiddleware
 from slowapi.util import get_remote_address
 
-limiter = Limiter(key_func=get_remote_address)
 app = FastAPI()
-app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 app.add_middleware(SlowAPIMiddleware)
 
@@ -44,26 +33,16 @@ class Deploy:
             self.load_model()
 
         @self.app.post("/generate/")
-        @limiter.limit("5/minute")  # rate limit
         async def generate(prompt: Prompt):
             return self.generate(prompt)
-
-        @self.app.get("/items/")
-        async def read_items(params: PaginationParams = Depends()):
-            items = [{"item": "item1"}, {"item": "item2"}, {"item": "item3"}]  # example items
-            return paginate(items)
-
-        @self.app.get("/download/")
-        async def download_file():
-            return FileResponse('path_to_file', filename='filename')
-
+        
     def load_model(self):
         global inference
         inference = self.llm
 
     async def generate(self, prompt: Prompt):
         try:
-            return {"generated_text": inference.run(prompt.text, prompt.max_length)}
+            return {"generated_text": inference.run(prompt.text)}
         except Exception as e:
             raise HTTPException(status_code=500, detail=str(e))
 
