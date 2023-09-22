203a05af108ed61d774a6b602dbf116a4550b200
Kye <kye@apacmediasolutions.com>
2023-09-22 16:02:52 -0400

    cleanup

--- exa/__init__.py
+++ exa/__init__.py
@@ -1,10 +1,9 @@
 from exa.inference.gptq import GPTQInference
 from exa.inference.hf import Inference
 from exa.quant.main import Quantize
-
-
 from exa.inference.text_to_video import TextToVideo
 from exa.inference.mmi import MultiModalInference
+from exa.inference.kosmos import Kosmos
 
 #utils
 from exa.utils.metric_logger import Logging
