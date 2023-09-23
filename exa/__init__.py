b2e7806ffa4edc0248d0a17a9029848b3333330f
Kye <kye@apacmediasolutions.com>
2023-09-23 10:44:07 -0400

    cleanup

--- exa/__init__.py
+++ exa/__init__.py
@@ -1,12 +1,12 @@
+from exa.inference.ctransformers import CInference
 from exa.inference.gptq import GPTQInference
 from exa.inference.hf import Inference
-from exa.quant.main import Quantize
-from exa.inference.text_to_video import TextToVideo
-from exa.inference.mmi import MultiModalInference
 from exa.inference.kosmos import Kosmos
-from exa.inference.ctransformers import CInference
+from exa.inference.mmi import MultiModalInference
+from exa.inference.text_to_video import TextToVideo
+from exa.quant.main import Quantize
+from exa.utils.decoding_wrapper import real_time_decoding
+from exa.utils.deploy import Deploy
 
 #utils
 from exa.utils.metric_logger import Logging
-from exa.utils.decoding_wrapper import real_time_decoding
-from exa.utils.deploy import Deploy
