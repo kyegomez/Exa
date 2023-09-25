a84a0f7de69e0f970ed3f923cc9ccd5aaee9a0d2
Kye <kye@apacmediasolutions.com>
2023-09-25 19:29:59 -0400

    initg

--- exa/__init__.py
+++ exa/__init__.py
@@ -6,17 +6,22 @@ print(exa)
 
 os.environ["HF_HUB_ENABLE_HF_TRANSFER"] = True
 
+telemetry = os.environ.get("TELEMETRY", "False")
 
-sentry_sdk.init(
-    dsn="https://c85aae98407fc178f819e13cd8506084@o4504578305490944.ingest.sentry.io/4505943647191040",
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    traces_sample_rate=1.0,
-    # Set profiles_sample_rate to 1.0 to profile 100%
-    # of sampled transactions.
-    # We recommend adjusting this value in production.
-    profiles_sample_rate=1.0,
-)
+if telemetry == "True":
+        
+    sentry_sdk.init(
+        dsn="https://c85aae98407fc178f819e13cd8506084@o4504578305490944.ingest.sentry.io/4505943647191040",
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        traces_sample_rate=1.0,
+        # Set profiles_sample_rate to 1.0 to profile 100%
+        # of sampled transactions.
+        # We recommend adjusting this value in production.
+        profiles_sample_rate=1.0,
+    )
+else:
+    pass
 
 #inference
 # from exa.inference.ctransformers import CInference
