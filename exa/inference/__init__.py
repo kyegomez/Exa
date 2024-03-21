054f0ac6d9982b82af38a72efe35ac9a37ccaedb
Kye <kye@apacmediasolutions.com>
2024-03-20 21:41:45 -0700

    [CLEANUP]

--- exa/inference/__init__.py
+++
@@ -1,6 +0,0 @@
-from exa.inference.gptq import GPTQInference
-from exa.inference.hf import Inference
-from exa.quant.main import Quantize
-
-from exa.inference.text_to_video import TextToVideo
-from exa.inference.mmi import MultiModalInference
--- example.py
+++
@@ -1,5 +0,0 @@
-from exa import Inference
-
-model = Inference(model_id="georgesung/llama2_7b_chat_uncensored", quantize=True)
-
-model.run("What is your name")
